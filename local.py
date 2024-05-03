face22301164b535adfba3f913cab2af77b9b689
Piter Kononihin <Piterbog@gmail.com>
2024-05-03 09:57:46 +0300

    new files

--- local.py
+++ local.py
@@ -1,6 +1,31 @@
 import requests
+import urllib.parse
+from apikey import API_TOKEN
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+key = API_TOKEN
 
 
-response = requests.get("http://127.0.0.1:4000/api/main")
+while True:
+    orig = input("Starting location : ")
+    if orig == "quit" or orig == "q":
+        break
+    dest = input("Destination : ")
+    if dest == "quit" or dest == "q":
+        break
 
-print(response.json())
+    url = main_api + \
+        urllib.parse.urlencode(
+            {"key": key, "from": orig, "to": dest, "unit": "k", "locale": "ru_RU"})
+
+    json_data = requests.get(url).json()
+    json_status = json_data['info']['statuscode']
+
+    print(f"URL : {url}")
+
+    if json_status == 0:
+        print(f"API Status : {str(json_status)}  = A successful route call.\n")
+
+    else:
+        json_status != 0
+        print("Error")
