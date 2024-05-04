1d924787f4fd490a169b96031481bdb33776d7a6
Piter Kononihin <Piterbog@gmail.com>
2024-05-04 08:25:41 +0300

    last changes in repo

--- local.py
+++ local.py
@@ -7,6 +7,12 @@ key = API_TOKEN
 
 
 while True:
+    print("Wellcome to my Navigator")
+
+    print("You need write 2 parameters for create routes.")
+
+    print("All time you can exit just write ( quit ) or ( q )")
+    print(50 * "=")
     orig = input("Starting location : ")
     if orig == "quit" or orig == "q":
         break
@@ -16,16 +22,42 @@ while True:
 
     url = main_api + \
         urllib.parse.urlencode(
-            {"key": key, "from": orig, "to": dest, "unit": "k", "locale": "ru_RU"})
+            {"key": key, "from": orig, "to": dest, "unit": "k"})
 
     json_data = requests.get(url).json()
     json_status = json_data['info']['statuscode']
-
-    print(f"URL : {url}")
+    json_messages = json_data['info']['messages']
 
     if json_status == 0:
-        print(f"API Status : {str(json_status)}  = A successful route call.\n")
+        print(75 * '==')
+        print(f"URL address     : {url}")
+        print(75 * '==')
+        print(
+            f"API Status is   : {json_status} successful route call.\n")
 
+        print(f"Directions from : {orig}")
+        print(f"Directions to   : {dest}")
+        print(f"Trip duration   : {json_data['route']['formattedTime']} time")
+        print(f"Kilometers      : {json_data['route']['distance']} Km")
+        print(f"Fuel used       : {json_data['route']['distance']*10/100} ltr")
+        print(60 * "==")
+
+        for gps in json_data['route']['legs'][0]['maneuvers']:
+            print((gps['narrative']) +
+                  " --- " + (str("{:.2f}".format(gps['distance']*1.58) + "km)")))
+            print(60 * "==")
+
+    elif json_status == 402:
+        print(40 * '**')
+        print(
+            f"Status coode : {json_status} Invalid user imputs for one or both locations.")
+        print(f"MapQuest message {json_messages}")
+        print(40 * '**')
+        break
     else:
-        json_status != 0
-        print("Error")
+        print(40 * '**')
+        print(f"MapQuest message {json_messages}")
+        print(f"Status code: {json_status} Refer to.")
+        print("URL for help :" +
+              "https://developer.mapquest.com/documentation/directions-api/status-codes")
+        print(40 * '**')
